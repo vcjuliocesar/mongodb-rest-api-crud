b8c624f64cba2499d60f47a4f6686b1186fd1e78
vcjuliocesar <vcjuliocesar@outlook.com>
2022-12-23 23:15:41 -0600

    feat: create model test

---
+++ tests/_support/Models/UserModelTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Support\Models;
+use App\Libraries\MongoDB;
+use CodeIgniter\Test\CIUnitTestCase;
+
+class UserModelTest extends CIUnitTestCase
+{
+    public function testStore()
+    {
+        d("Hola");
+    }
+}
--- tests/database/ExampleDatabaseTest.php
+++ tests/database/ExampleDatabaseTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use CodeIgniter\Test\CIUnitTestCase;
-use CodeIgniter\Test\DatabaseTestTrait;
 use Tests\Support\Database\Seeds\ExampleSeeder;
 use Tests\Support\Models\ExampleModel;
 
@@ -10,36 +9,38 @@ use Tests\Support\Models\ExampleModel;
  */
 final class ExampleDatabaseTest extends CIUnitTestCase
 {
-    use DatabaseTestTrait;
-
-    protected $seed = ExampleSeeder::class;
+    //use DatabaseTestTrait;
+    protected $seed;
 
     public function testModelFindAll()
     {
+        (new ExampleSeeder)->run();
         $model = new ExampleModel();
-
         // Get every row created by ExampleSeeder
-        $objects = $model->findAll();
-
+        $objects = $model->getCollection()->find();
         // Make sure the count is as expected
         $this->assertCount(3, $objects);
     }
 
     public function testSoftDeleteLeavesRow()
     {
+        (new ExampleSeeder)->run();
         $model = new ExampleModel();
-        $this->setPrivateProperty($model, 'useSoftDeletes', true);
-        $this->setPrivateProperty($model, 'tempUseSoftDeletes', true);
 
-        $object = $model->first();
-        $model->delete($object->id);
+        $object = $model->getCollection()->findOne();
+        $model->getCollection()->deleteOne(['_id' => new \MongoDB\BSON\ObjectId($object->_id)]);
 
         // The model should no longer find it
-        $this->assertNull($model->find($object->id));
+        $this->assertNull($model->getCollection()->findOne(['_id'=> new \MongoDB\BSON\ObjectId($object->_id)]));
+    }
 
-        // ... but it should still be in the database
-        $result = $model->builder()->where('id', $object->id)->get()->getResult();
+    protected function tearDown(): void
+    {
+        parent::tearDown();
 
-        $this->assertCount(1, $result);
+        // Do something here....
+        $model = new ExampleModel();
+        $model->deleteCollection();
+        //(new TestMongoDb())->tearDown();
     }
 }
